60e356a7895a3694eaea5b5002c628bca6e658a0
Noah Gilmore <noah.w.gilmore@gmail.com>
2019-10-26 15:28:53 -0700

    Initial Commit

---
+++ MetalByExampleExamples/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MetalByExampleExamples
+//
+//  Created by Noah Gilmore on 10/26/19.
+//  Copyright © 2019 Noah Gilmore. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
