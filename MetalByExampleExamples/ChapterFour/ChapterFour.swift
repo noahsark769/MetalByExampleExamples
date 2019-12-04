90280a82df3ca4f7a9b4726cde633bcfd5e15b41
Noah Gilmore <noah.w.gilmore@gmail.com>
2019-12-04 08:43:39 -0800

    Use MDL transforms

--- MetalByExampleExamples/ChapterFour/ChapterFour.swift
+++ MetalByExampleExamples/ChapterFour/ChapterFour.swift
@@ -155,10 +155,10 @@ final class ChapterFourRenderer: NSObject {
 
     func updateUniformsForView(aspectRatio: Float) {
         self.camera.sensorAspect = aspectRatio
-        let modelToWorld =
-            matrix4x4_translation(0, 0, -5) *
-            matrix4x4_rotation(radians: radians_from_degrees(45), axis: SIMD3<Float>(1, 0, 0)) *
-            matrix4x4_rotation(radians: radians_from_degrees(45), axis: SIMD3<Float>(0, 1, 0))
+        let transform = MDLTransform()
+        transform.setTranslation(SIMD3<Float>(0, 0, -5), forTime: 0)
+        transform.setRotation(SIMD3<Float>(radians_from_degrees(45), radians_from_degrees(45), 0), forTime: 0)
+        let modelToWorld = transform.localTransform(atTime: 0)
         self.worldToView = self.camera.projectionMatrix
         let uniforms = [ChapterFourUniforms(modelViewProjection: self.worldToView * modelToWorld)]
         memcpy(self.uniformsBuffer.contents(), UnsafeRawPointer(uniforms), MemoryLayout<ChapterFourUniforms>.size)
@@ -233,35 +233,6 @@ struct ChapterFour: View {
             ChapterFourRepresentable().gesture(gesture)
         }
     }
-}
-
-func matrix4x4_rotation(radians: Float, axis: SIMD3<Float>) -> matrix_float4x4 {
-    let unitAxis = normalize(axis)
-    let ct = cosf(radians)
-    let st = sinf(radians)
-    let ci = 1 - ct
-    let x = unitAxis.x, y = unitAxis.y, z = unitAxis.z
-    return matrix_float4x4.init(columns:(vector_float4(    ct + x * x * ci, y * x * ci + z * st, z * x * ci - y * st, 0),
-                                         vector_float4(x * y * ci - z * st,     ct + y * y * ci, z * y * ci + x * st, 0),
-                                         vector_float4(x * z * ci + y * st, y * z * ci - x * st,     ct + z * z * ci, 0),
-                                         vector_float4(                  0,                   0,                   0, 1)))
-}
-
-func matrix4x4_translation(_ translationX: Float, _ translationY: Float, _ translationZ: Float) -> matrix_float4x4 {
-    return matrix_float4x4.init(columns:(vector_float4(1, 0, 0, 0),
-                                         vector_float4(0, 1, 0, 0),
-                                         vector_float4(0, 0, 1, 0),
-                                         vector_float4(translationX, translationY, translationZ, 1)))
-}
-
-func matrix_perspective_right_hand(fovyRadians fovy: Float, aspectRatio: Float, nearZ: Float, farZ: Float) -> matrix_float4x4 {
-    let ys = 1 / tanf(fovy * 0.5)
-    let xs = ys / aspectRatio
-    let zs = farZ / (nearZ - farZ)
-    return matrix_float4x4.init(columns:(vector_float4(xs,  0, 0,   0),
-                                         vector_float4( 0, ys, 0,   0),
-                                         vector_float4( 0,  0, zs, -1),
-                                         vector_float4( 0,  0, zs * nearZ, 0)))
 }
 
 func radians_from_degrees(_ degrees: Float) -> Float {
